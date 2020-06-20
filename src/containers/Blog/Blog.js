250dfbb0aca6fbe02e7716bacecedfc059ba8757
Omer Kahoot <omer@mauqa.online>
2020-06-20 06:59:18 +0500

    initial data fetch from dummy api

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -3,9 +3,17 @@ import React, { Component } from 'react';
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
+import axios from 'axios';
 import './Blog.css';
 
 class Blog extends Component {
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+             .then(response => {
+                 console.log(response);
+             });
+    }
+    
     render () {
         return (
             <div>
